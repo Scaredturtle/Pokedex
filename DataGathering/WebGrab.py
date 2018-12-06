39c829368234b97f77edc2b113c5b481fb9200e3
Scaredturtle <demarcam2012@yahoo.com>
2018-12-06 14:49:39 -0500

    Working data grab from the web
    Using pandas I am now able to pull stat tables from the web and save them as a .csv

--- DataGathering/WebGrab.py
+++ DataGathering/WebGrab.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import os.path
-import pandas
+import pandas as pd
 import json
 import requests
 from bs4 import BeautifulSoup
@@ -9,6 +9,7 @@ import pprint
 
 class Gather:
     def webGet(self):
+        '''
         print("retrieving data from web")
         url = 'https://pokemondb.net/pokedex/' + self.pokemon.lower()
         rawHTML = requests.get(url)
@@ -28,15 +29,33 @@ class Gather:
             
 
         testFile.close()
+        '''
+        rawHTML = requests.get(self.url, headers={'User-Agent': 'Mozilla/5.0'})
+        testtable = pd.read_html(rawHTML.text)
+        #print(testtable[3])
+        rawTable = testtable[3]
+        #testtable[3].to_csv(self.csvFile, index=None)
+        if len(rawTable.columns) == 7:
+            finalTable = rawTable.drop(rawTable.columns[[2, 5, 6]], axis = 1)
+        if len(rawTable.columns) == 6:
+            finalTable = rawTable.drop(rawTable.columns[[2, 5]], axis = 1)
+        if len(rawTable.columns) <= 5:
+            finalTable = rawTable.drop(rawTable.columns[2], axis = 1)
+        
+        print(finalTable)
+        finalTable.to_csv(self.csvFile, index = None)
 
     def dataCheck(self, pokemon):
-        self.pokemon = pokemon
+        self.pokemon = pokemon.lower()
         fileDirectory = ".\\data\\"
 
         print(self.pokemon)
 
-        if os.path.exists(fileDirectory + pokemon.lower() + ".json"):
-            print("True")
+        if os.path.exists(fileDirectory + pokemon.lower() + ".csv"):
+            print("Data already exists in the database for this Pokemon.")
         else:
             print(fileDirectory + pokemon.lower() +".json File Not Found!")
+            self.url = 'https://pokemondb.net/pokedex/' + self.pokemon
+            self.csvFile = fileDirectory + str(self.pokemon) + ".csv"
+            print(self.url, self.csvFile)
             self.webGet()
